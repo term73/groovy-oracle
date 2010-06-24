be81a05ca6b7930ef3f8b36919067e54e8ddc9b6
andre.winkler@web.de <andre.winkler@web.de@c2fd887a-2c4e-11de-a0ce-9586e7784243>
2010-06-24 15:09:02 +0000

    TAG of grooorcl-0.4.0

---
+++ groovy-oracle/tags/0.4.0-release/src/main/groovy/de/awtools/grooocle/meta/ForeignKey.groovy
@@ -0,0 +1,84 @@
+/*
+ * $Id$
+ * ============================================================================
+ * Project grooocle
+ * Copyright (c) 2008-2010 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.awtools.grooocle.meta
+
+/**
+ * Informationen über einen Fremdschlüsselbeziehung in einer Oracle-Datenbank.
+ */
+class ForeignKey {
+
+    String name
+    String rConstraintName
+    def columnNames = []
+    String tableName
+    String referencedTableName
+    def referencedColumnNames = []
+
+    /**
+     * Kopiert die Eigenschaften von diesem Objekt in ein neue erstelltes
+     * Objekt 'ForeignKey'
+     *
+     * @return Ein geclonter ForeignKey.
+     */
+    def copy() {
+        def foreignKey = new ForeignKey(name: name,
+                rConstraintName: rConstraintName,
+                tableName: tableName,
+                referencedTableName: referencedTableName)
+
+        def columnNames2 = []
+        columnNames.each { columnNames2 << it }
+
+        def referencedColumnNames2 = []
+        referencedColumnNames.each { referencedColumnNames2 << it }
+
+        foreignKey.columnNames = columnNames2
+        foreignKey.referencedColumnNames = referencedColumnNames2
+
+        return foreignKey
+    }
+
+    String toString() {
+    	return "${tableName}#${name}" 
+    }
+
+    boolean equals(def object) {
+        if (!(object instanceof ForeignKey)) {
+            return false
+        }
+
+        def result = true
+        result = result && (name == object.name)
+        result = result && (rConstraintName == object.rConstraintName)
+        result = result && (tableName == object.tableName)
+        result = result && (referencedTableName == object.referencedTableName)
+        result = result && (columnNames == object.columnNames) 
+        result = result && (referencedColumnNames == object.referencedColumnNames)
+
+        return result
+    }
+
+}
---
+++ groovy-oracle/tags/0.4.0-release/src/main/groovy/de/awtools/grooocle/meta/OracleSequenceFinder.groovy
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ * ============================================================================
+ * Project grooocle
+ * Copyright (c) 2008-2010 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.awtools.grooocle.meta
+
+/**
+ * Liefert die Namen der Oracle Sequenzen.
+ * 
+ * @author  $Author$
+ * @version $Revision$ $Date$
+ */
+class OracleSequenceFinder {
+	
+	/**
+	 * Liefert eine Liste aller Sequenzen.
+	 *
+	 * @param sql Ein Groovy SQL
+	 * @return Eine Liste von Sequenz-Namen.
+	 */
+	def getSequences(def sql) {
+		def sequences = []
+		sql.eachRow("""
+            select
+                object_name as name
+            from
+                user_objects
+            where
+                object_type = 'SEQUENCE'
+            order by
+                object_name
+        """) {
+			sequences << it.name
+		}
+		return sequences
+	}
+	
+}
